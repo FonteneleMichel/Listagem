2fe3e748b352e10e06c1d9fcbab88015c3dfb725
fontenelemichel <fontenele.dev@gmail.com>
2023-05-19 14:36:45 -0300

    ListActivity added and Gradle updated

--- app/src/main/java/com/sdk/listagemapp/view/ListActivity.kt
+++ app/src/main/java/com/sdk/listagemapp/view/ListActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.sdk.listagemapp.model.User
 import com.sdk.listagemapp.GitHubService
 import com.sdk.listagemapp.R
+import com.sdk.listagemapp.model.User
 import com.sdk.listagemapp.viewmodel.UserAdapter
 import retrofit2.Call
 import retrofit2.Callback
@@ -14,13 +14,13 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class ListagemActivity : AppCompatActivity() {
+class ListActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: UserAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_listagem)
+        setContentView(R.layout.activity_list)
 
         recyclerView = findViewById(R.id.recyclerView)
         adapter = UserAdapter()
